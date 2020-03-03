b3b56afdfb8962143f28f006ccb6790760099a58
Adrian <adrian@adrianpadilla.net>
2020-03-02 19:24:06 -0800

    Added projects

---
+++ src/console-app/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        internal static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+}
